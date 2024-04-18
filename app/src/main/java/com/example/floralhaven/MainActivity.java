ea180cd97534ae707cc99dd84d40290473cc89dd
harutlem <hlementsyan@csumb.edu>
2024-04-18 16:08:29 -0700

    Added Log in Screen

--- app/src/main/java/com/example/floralhaven/MainActivity.java
+++ app/src/main/java/com/example/floralhaven/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.floralhaven;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,20 +16,46 @@ import kotlinx.coroutines.flow.Flow;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String MAIN_ACTIVITY_USER_ID = "com.example.floralhaven.MAIN_ACTIVITY_USER_ID";
     ActivityMainBinding binding;
 
     private FlowerRepository repository;
 
     public static final String TAG = "HL_FLOWER";
+
+    int loggedInUserId = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        loginUser();
+
+        if(loggedInUserId == -1)
+        {
+            Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+            startActivity(intent);
+        }
+
         repository = FlowerRepository.getRepository(getApplication());
 
 
 
     }
+
+    private void loginUser() {
+        //Todo: create login method
+        loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, -1);
+    }
+
+    static Intent mainActivityIntentFactory(Context context, int userId)
+    {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
+        return intent;
+    }
+
+
+
 }
